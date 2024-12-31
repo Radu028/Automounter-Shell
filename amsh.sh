3f3862f6748bb98af34a470664c41e56b6e88a64
Radu <radupopa028@gmail.com>
2024-12-31 17:29:18 +0200

    Update custom_cd function

--- amsh.sh
+++ amsh.sh
@@ -6,28 +6,14 @@ echo "Welcome to amsh shell!"
 custom_cd() {
     local target_dir="$1"
 
-    # Check if the directory exists
+    # Check if the target directory exists
     if [ ! -d "$target_dir" ]; then
         echo "Directory not found: $target_dir"
         return 1
     fi
 
-    # Check if the target directory passes through a mountpoint
-    while read -r mountpoint device filesystem options; do
-        if [[ "$target_dir" == "$mountpoint"* ]]; then
-
-            # Cehck if the mountpoint is already mounted
-            if ! mount | grep -q "$mountpoint"; then
-                echo "Mounting $mountpoint..."
-                sudo mount "$device" "$mountpoint"
-                if [ $? -ne 0 ]; then
-                    echo "Failed to mount $mountpoint"
-                    return 1
-                fi
-            fi
-
-        fi
-    done <config/fstab_similar
+    # Try to mount if necessary
+    auto_mount "$target_dir"
 
     # Change the directory
     cd "$target_dir" || return 1
